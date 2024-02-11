4d4b6313ac6645dafbbeafe9b95324616ed96f51
maxim danilov <abl1v1on@yandex.ru>
2024-02-11 14:37:05 +0500

    add loader

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,30 @@
             return {
                 city: '',
                 weather_info: '',
-                weather_icon: ''
+                weather_icon: '',
+                isLoading: false
             }
         },
 
         methods: {
             async sendRequest() {
-                try {
+                try {    
+                    this.isLoading = true
+
                     const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=85c9a66fb2a3bfaf771efe7e71769311&lang=ru&units=metric`)
                     console.log(data)
+
                     this.weather_info = data
                     this.weather_icon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
                 } catch (ex) {
                     this.weather_info = 'Город не найден'
+                } finally {
+                    this.isLoading = false
                 }
             }
         }
     }
-</script>
+</script>   
 
 <template>
 <div style="max-width: 1000px; margin: 0 auto; margin-top: 170px;" class="container">
@@ -34,7 +40,14 @@
             </div>
             <button @click="sendRequest" style="max-width: 300px; margin: 0 auto;" class="btn btn-outline-info w-100" type="button">Узнать погоду</button>
     </div>
-        <div v-if="weather_info !== '' && weather_info !== 'Город не найден'" class="card shadow-0 border" style="max-width: 300px; margin: 0 auto;">
+        <!-- loader -->
+        <div v-if="isLoading" style="text-align: center ;">
+            <div class="loader">
+                <div class="loader_inner"></div>
+            </div>
+        </div>
+
+        <div v-show="!isLoading" v-if="weather_info !== '' && weather_info !== 'Город не найден'" class="card shadow-0 border" style="max-width: 300px; margin: 0 auto;">
             <div class="card-body p-4">
                 <h4 class="mb-1 sfw-normal"></h4>
                 <p class="mb-2">Текущая температура: <strong>{{ weather_info.main.temp }}°C</strong></p>
@@ -56,5 +69,27 @@
 </template>
 
 <style scoped>
-
+.loader {
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  z-index: 9999;
+}
+ 
+.loader .loader_inner {
+  background-image: url("../public/loader.gif");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  height: 60px;
+  width: 60px;
+  margin-top: -30px;
+  margin-left: -30px;
+  left: 50%;
+  top: 50%;
+  position: absolute;
+}
 </style>
---
+++ src/loader-script.js
@@ -0,0 +1,4 @@
+$(window).on("load", function() {
+    $(".loader_inner").fadeOut(1000);
+    $(".loader").delay(1000).fadeOut("slow");
+  });
